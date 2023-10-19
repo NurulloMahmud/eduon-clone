78ea7e8a554e0837ab1680f8ad7cb1941e8c4108
nurulloh <nurullomahmud@gmail.com>
2023-10-19 09:55:50 +0500

    db and main app added

--- root/settings.py
+++ root/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom
+    'main',
+
+    # third party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
